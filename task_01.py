57cd8a718ffce1205d0f338eade97931349a33d7
pinto921 <pinto921@hotmail.com>
2016-09-30 04:13:01 +0000

    task 01

---
+++ task_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf- -*-
+
+"""Tuples"""
+
+ELEMENTS = (None, "hydrogen", "helium", "lithium", "berllium", "boron", "carbon")
+
+OPERATIONS_ORDER = ["paranthesis", "exponents", "multiplication", "division", "addition", "substraction"]
